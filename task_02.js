4c5bb00621d647d48bdc5a6ac739f82b67da4980
Gusonkad <gusonkad@gmail.com>
2021-04-01 16:30:34 +0300

    Add second task to js_hw_04

--- task_02.js
+++ task_02.js
@@ -1,7 +1,24 @@
 // task_02.js
-const countProps = function(obj) {
-   return Object.keys(obj).length;
-}
-console.log(countProps({})); // 0
-console.log(countProps({ name: 'Mango', age: 2 })); // 2
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+const inventory = {
+   items: ['Knife', 'Gas mask'],
+   add(itemName) {
+      console.log(`Adding ${itemName} to inventory`);
+      this.items.push(itemName);
+   },
+   remove(itemName) {
+      console.log(`Removing ${itemName} from inventory`);
+      this.items = this.items.filter(item => item !== itemName);
+   },
+};
+const invokeInventoryAction = function (itemName, action) {
+   console.log(`Invoking action on ${itemName}`);
+   action.call(inventory, itemName); //Нужно было указать обьект: inventory и вызвать метод: call
+};
+invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+console.log(inventory.items); // ['Knife', 'Medkit']
